f528d92dbeb342c47b73c68bed116a87f5182bd7
LiuTao <liutao_2020@csii.com.cn>
2020-07-07 21:50:05 +0800

    55

---
+++ demo/src/main/java/Application.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 刘涛 on 2020/07/07.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
